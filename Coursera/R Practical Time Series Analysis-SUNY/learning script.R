a961fb5ec7fcdce47eaaf20de7995f8c0cff71cb
pbdanny <pb.danny@gmail.com>
2018-03-20 05:22:14 +0700

    day 8

--- Coursera/R Practical Time Series Analysis-SUNY/learning script.R
+++ Coursera/R Practical Time Series Analysis-SUNY/learning script.R
@@ -387,3 +387,60 @@ phi0_hat <- mean(my_data)*(1 - sum(phi_hat))
 phi0_hat
 
 cat("Constant:", phi0_hat," Coeffcinets:", phi_hat, " and Variance:", var_hat, '\n')
+
+
+## Johnson & Johnson quarterly earnings per share
+library(astsa)
+
+# Time plot for Johnson&Johnson
+plot(JohnsonJohnson, main = 'Johnson&Johnosn earnings per share', 
+     col = 'blue', lwd = 3)
+
+# log-return of Johnson&Johnson
+jj.log.return <- diff(log(JohnsonJohnson))
+jj.log.return.mean.zero <- jj.log.return-mean(jj.log.return)
+
+# Plots for log-returns
+par(mfrow = c(3,1))
+plot(jj.log.return.mean.zero, main = 'Log-return (mean zero) of Johnson&Johnosn earnings per share')
+acf(jj.log.return.mean.zero, main = 'ACF')
+pacf(jj.log.return.mean.zero, main = 'PACF')
+
+# Order
+p <- 4
+
+# sample autocorreleation function r
+r <- NULL
+r[1:p] <- acf(jj.log.return.mean.zero, plot = F)$acf[2:(p+1)]
+r
+
+# matrix R
+R <- matrix(1,p,p) # matrix of dimension 4 by 4, with entries all 1's.
+
+# define non-diagonal entires of R
+for(i in 1:p){
+  for(j in 1:p){
+    if(i != j)
+      R[i,j] <- r[abs(i-j)]
+  }
+}
+R
+
+# b-column vector on the right
+b <- matrix(r,p,1) # b- column vector with no entries
+b
+
+phi.hat <- solve(R,b)[,1]
+phi.hat
+
+# Variance estimation using Yule-Walker Estimator
+c0 <- acf(jj.log.return.mean.zero, type = 'covariance', plot = F)$acf[1]
+c0
+var.hat <- c0*(1 - sum(phi.hat*r))
+var.hat
+
+# Constant term in the model
+phi0.hat <- mean(jj.log.return)*(1 - sum(phi.hat))
+phi0.hat
+
+cat("Constant:", phi0.hat," Coeffcinets:", phi.hat, " and Variance:", var.hat, '\n')
